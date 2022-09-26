d4c4e649f51718b85c2c53433e640ce85b9e4886
pietromosca1994 <pietromosca1994@gmail.com>
2022-09-26 14:51:35 +0200

    added hello_cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-12c687c84e48e3ac.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/pietr/Desktop/repos/rust_by_example/hello_cargo/target/debug/deps/hello_cargo-12c687c84e48e3ac.rmeta: src/main.rs
+
+/mnt/c/Users/pietr/Desktop/repos/rust_by_example/hello_cargo/target/debug/deps/hello_cargo-12c687c84e48e3ac.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/pietr/Desktop/repos/rust_by_example/hello_cargo/target/debug/hello_cargo: /mnt/c/Users/pietr/Desktop/repos/rust_by_example/hello_cargo/src/main.rs
